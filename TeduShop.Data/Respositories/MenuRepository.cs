2f50e3f58cccccfd4689985ccac7ed79cd90590c
DATPM/DATPM <zac22598@gmail.com>
2021-04-22 14:50:57 +0700

    Bai7 Fixed

---
+++ TeduShop.Data/Respositories/MenuRepository.cs
@@ -0,0 +1,17 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IMenuRepository : IRepository<Menu>
+    {
+        //add something Method
+    }
+    public class MenuRepository : RepositoryBase<Menu>, IMenuRepository
+    {
+        public MenuRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
---
+++ TeduShop.Data/Respositories/SlideRepository.cs
@@ -0,0 +1,18 @@
+﻿using TeduShop.Model.Models;
+using TeduShop.Data.Infrastructure;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface ISlideRepository : IRepository<Slide>
+    {
+
+    }
+    public class SlideRepository : RepositoryBase<Slide>, ISlideRepository
+    {
+        public SlideRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+
+}
