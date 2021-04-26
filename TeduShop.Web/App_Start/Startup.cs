75c1e6f4a9579f3a564348bb3488265c469315d1
DATPM/DATPM <zac22598@gmail.com>
2021-04-26 16:48:29 +0700

    Bai 15 Fix

--- TeduShop.Web/App_Start/Startup.cs
+++ TeduShop.Web/App_Start/Startup.cs
@@ -12,16 +12,21 @@ using TeduShop.Service;
 using System.Web.Http;
 using System.Web.Mvc;
 using Autofac.Integration.WebApi;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.DataProtection;
+using TeduShop.Model.Models;
 
 [assembly: OwinStartup(typeof(TeduShop.Web.App_Start.Startup))]
 
 namespace TeduShop.Web.App_Start
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             ConfigAutofac(app);
+            ConfigureAuth(app);
         }
 
         private void ConfigAutofac(IAppBuilder app)
@@ -36,14 +41,13 @@ namespace TeduShop.Web.App_Start
 
             builder.RegisterType<TeduShopDBContext>().AsSelf().InstancePerRequest();
 
-            ////Asp.net Identity
-            //builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
-            //builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
-            //builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
-            //builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
-            //builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
-
-
+            //Asp.net Identity
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
+            
             // Repositories
             builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
                 .Where(t => t.Name.EndsWith("Repository"))
