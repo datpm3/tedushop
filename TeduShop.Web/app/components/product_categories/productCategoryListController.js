6c152b004290d1c2d8fc224a02773b179a85b21f
DATPM/DATPM <zac22598@gmail.com>
2021-05-11 10:29:55 +0700

    bai26

--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -5,27 +5,34 @@
         $scope.productCategories = [];
 
         $scope.page = 0;
-        $scope.pagesCount = 0; 
+        $scope.pagesCount = 0;
         $scope.getProductCategories = getProductCategories;
+        $scope.keyword = '';
 
-        function getProductCategories(page) {
-            page = page || 0;
-            var config = {
-                params: {
-                    page: page,
-                    pageSize : 20
-                }
-            }
-            apiService.get('/api/productcategory/getall', config, function (result) {
-                $scope.productCategories = result.data.Items;
-                $scope.page = result.data.Page;
-                $scope.pagesCount = result.data.TotalPages;
-                $scope.totalCount = result.data.TotalCount;
-            }, function () {
-                console.log('Load Productcategory failed.');
-            });
+        $scope.search = search;
+
+        function search() {
+            getProductCategories();
         }
-        $scope.getProductCategories();
-    }
+            function getProductCategories(page) {
+                page = page || 0;
+                var config = {
+                    params: {
+                        keyword: $scope.keyword,
+                        page: page,
+                        pageSize: 20
+                    }
+                }
+                apiService.get('/api/productcategory/getall', config, function (result) {
+                    $scope.productCategories = result.data.Items;
+                    $scope.page = result.data.Page;
+                    $scope.pagesCount = result.data.TotalPages;
+                    $scope.totalCount = result.data.TotalCount;
+                }, function () {
+                    console.log('Load Productcategory failed.');
+                });
+            }
+            $scope.getProductCategories();
+        }
 
-})(angular.module('tedushop.product_categories'));
+    })(angular.module('tedushop.product_categories'));
