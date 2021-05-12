8c282d6a7687d81dbdfb66313af6788f433186ee
DATPM/DATPM <zac22598@gmail.com>
2021-05-12 10:27:36 +0700

    bai 27 notification

--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,7 +1,7 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
-    productCategoryListController.$inject = ['$scope', 'apiService'];
-    function productCategoryListController($scope, apiService) {
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
 
         $scope.page = 0;
@@ -24,10 +24,18 @@
                     }
                 }
                 apiService.get('/api/productcategory/getall', config, function (result) {
+                    
+                    if (result.data.TotalCount == 0) {
+                        notificationService.displayWarning('Không có bản ghi nào được tìm thấy.');
+                    }
+                    else {
+                        notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount + ' bản ghi.');
+                    }
                     $scope.productCategories = result.data.Items;
                     $scope.page = result.data.Page;
                     $scope.pagesCount = result.data.TotalPages;
                     $scope.totalCount = result.data.TotalCount;
+
                 }, function () {
                     console.log('Load Productcategory failed.');
                 });
---
+++ TeduShop.Web/app/shared/filter/statusFilter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('StatusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    });
+})(angular.module('tedushop.common'));
